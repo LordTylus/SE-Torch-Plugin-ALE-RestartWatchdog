caca38f798645f650c098d5428fce667762fe896
LordTylus <LordTylus@users.noreply.github.com>
2019-04-14 22:30:19 +0200

    Makes sure that if server reaches unloaded state that it still will restart.

--- RestartConfig.cs
+++ RestartConfig.cs
@@ -5,7 +5,10 @@ namespace ALE_RestartWatchdog {
     public class RestartConfig : ViewModel
     {
         private int _delayInSeconds = 60 * 2; //2 Minutes
+        private int _delayInSecondsAfterUnload = 60 * 1; //1 Minutes
 
         public int DelayInSeconds { get => _delayInSeconds; set => SetValue(ref _delayInSeconds, value); }
+
+        public int DelayInSecondsAfterUnload { get => _delayInSecondsAfterUnload; set => SetValue(ref _delayInSecondsAfterUnload, value); }
     }
 }
--- RestartManager.cs
+++ RestartManager.cs
@@ -43,60 +43,79 @@ namespace ALE_RestartWatchdog
         }
 
         private void SessionChanged(ITorchSession session, TorchSessionState state) {
-            
+
+            RestartConfig config = RestartPlugin.Instance.Config;
+
             switch (state) {
                 case TorchSessionState.Unloading:
 
                     Log.Info("Start Unloading!!!");
 
-                    RestartConfig config = RestartPlugin.Instance.Config;
+                    int delayUnloading = config.DelayInSeconds;
 
-                    int delay = config.DelayInSeconds;
-
-                    Log.Info("Wait for " + delay + " before force Restart!");
+                    Log.Info("Wait for " + delayUnloading + " before force Restart!");
 
                     /* Delay is in Seconds We need Milliseconds so * 1000 */
-                    Task.Delay(delay * 1000).ContinueWith((t) => {
-
-                        /* State is fine so no need to fix anything */
-                        if (session.State != TorchSessionState.Unloading) {
-                            Log.Info("Shutdown successful no need to interfere!");
-                            return;
-                        }
-                        
-                        Log.Warn("Unloading took too long. Force restart!");
-
-                        string exe = Assembly.GetEntryAssembly().Location;
-
-                        /* Making sure we found an EXE */
-                        if (exe == null || exe.Equals("")) {
-                            Log.Error("Could not find Path to exe file! Aborting Restart!");
-                            return;
-                        }
-
-                        try {
-
-                            var torchConfig = (TorchConfig) session.Torch.Config;
-                            /* Making sure Logger is done */
-                            LogManager.Flush();
-
-                            Process currentProcess = Process.GetCurrentProcess();
-
-                            /** Tell the new Torch instance to wait till the old one is gone */
-                            torchConfig.WaitForPID = currentProcess.Id.ToString();
-                            torchConfig.Autostart = true;
-
-                            Log.Info("Starting '" + exe + "' with '" + torchConfig.ToString() + "'!");
-                            Process.Start(exe, torchConfig.ToString());
-
-                            Log.Info("Killing current Process!");
-                            currentProcess.Kill();
-
-                        } catch(Exception e) {
-                            Log.Error("Could not Restart the Server! Tried running '" + exe + "' And Stopping PID " + Process.GetCurrentProcess().Id.ToString(), e);
-                        }
+                    Task.Delay(delayUnloading * 1000).ContinueWith((t) => {
+                        CheckRestart(session, TorchSessionState.Unloading);
                     });
                     break;
+
+                case TorchSessionState.Unloaded:
+
+                    Log.Info("Start Unloaded!!!");
+
+                    int delayUnloaded = config.DelayInSecondsAfterUnload;
+
+                    Log.Info("Wait for " + delayUnloaded + " before force Restart!");
+
+                    /* Delay is in Seconds We need Milliseconds so * 1000 */
+                    Task.Delay(delayUnloaded * 1000).ContinueWith((t) => {
+                        CheckRestart(session, TorchSessionState.Unloaded);
+                    });
+                    break;
+            }
+        }
+
+        private void CheckRestart(ITorchSession session, TorchSessionState referenceState) {
+
+
+            /* State is fine so no need to fix anything */
+            if (session.State != referenceState) {
+                Log.Info("Shutdown successful no need to interfere after "+ referenceState + "!");
+                return;
+            }
+
+            Log.Warn("Unloading took too long. Force restart!");
+
+            string exe = Assembly.GetEntryAssembly().Location;
+
+            /* Making sure we found an EXE */
+            if (exe == null || exe.Equals("")) {
+                Log.Error("Could not find Path to exe file! Aborting Restart!");
+                return;
+            }
+
+            try {
+
+                var torchConfig = (TorchConfig) session.Torch.Config;
+                /* Making sure Logger is done */
+                LogManager.Flush();
+
+                Process currentProcess = Process.GetCurrentProcess();
+
+                /** Tell the new Torch instance to wait till the old one is gone */
+                torchConfig.WaitForPID = currentProcess.Id.ToString();
+                torchConfig.Autostart = true;
+
+                Log.Info("Starting '" + exe + "' with '" + torchConfig.ToString() + "'!");
+                Process.Start(exe, torchConfig.ToString());
+
+                Log.Info("Killing current Process!");
+                currentProcess.Kill();
+
+            } catch (Exception e) {
+                Log.Error("Could not Restart the Server! Tried running '" + exe + "' And Stopping PID " + Process.GetCurrentProcess().Id.ToString(), e);
             }
         }
     }
